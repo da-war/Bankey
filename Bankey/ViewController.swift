da1e1670049eed17b8b68d7f37a6d009d1720e36
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-11-06 11:17:05 +0500

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Rana Dawar Abdullah on 06/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
