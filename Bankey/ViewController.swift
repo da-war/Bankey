efcd24c704a8fca1004a3309e1e448475b42f349
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-11-06 13:02:35 +0500

    Basic updates

--- Bankey/ViewController.swift
+++ Bankey/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  Bankey
-//
-//  Created by Rana Dawar Abdullah on 06/11/2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set up gradient background
+        setupGradientBackground()
+        
+        // Set up the welcome label
+        setupWelcomeLabel()
     }
-
-
+    
+    private func setupGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemPurple.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = view.bounds
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    private func setupWelcomeLabel() {
+        let welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome to the Cool App!"
+        welcomeLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        welcomeLabel.textColor = .white
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.numberOfLines = 0
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        
+        // Center the label in the view
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
 }
-
